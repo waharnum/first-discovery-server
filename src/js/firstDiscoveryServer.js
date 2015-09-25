da55ee09d50d061f9294a8ab850f1740bc80ae07
Justin Obara <obarajustin@gmail.com>
2015-09-25 15:06:39 -0400

    FLOE-420: Adding grunt for linting

--- src/js/firstDiscoveryServer.js
+++ src/js/firstDiscoveryServer.js
@@ -9,6 +9,8 @@ You may obtain a copy of the ECL 2.0 License and BSD License at
 https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 */
 
+"use strict";
+
 var fluid = fluid || require("infusion");
 var gpii = fluid.registerNamespace("gpii");
 
