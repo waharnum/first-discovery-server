7881c1cf1d8e35b96b4c1e0f80a58125904afe6d
Justin Obara <obarajustin@gmail.com>
2015-10-07 10:22:19 -0400

    FLOE-423: Updating comments

--- install.js
+++ install.js
@@ -13,8 +13,10 @@ https://github.com/fluid-project/first-discovery-server/raw/master/LICENSE.txt
 var child_process = require("child_process");
 
 // spawns the npm install
-// need to use --force to ensure that "dedupe-infusion" failures from gpii-express
-// do not prevent its installation.
+// Need to use --force to ensure that "dedupe-infusion" failures from gpii-express
+// do not prevent its installation. The dedupe-infusion postinstall task in gpii-express
+// is currently failing because the grunt-gpii dependency is not installed before
+// the postinstall script is run.
 var npm = child_process.spawn("npm", ["install", "--force"], {stdio: "inherit"});
 
 npm.on("close", function (exitCode) {
@@ -25,10 +27,13 @@ npm.on("close", function (exitCode) {
         dedupe.on("close", function (exitCode) {
             // if dedupe process exited normally
             if (exitCode !== null) {
-                // spanws the re-install of first-discovery
-                // this is needed because the deduplication removes its version of
-                // infusion as well, despite the fact that first-discovery only
-                // runs in the client
+                // spanws the reinstall of first-discovery
+                // first-discovery also depends on Infusion and contains a copy of
+                // Infusion wihtin its lib directory.
+                // When the deduplication process is run, this copy of Infusion
+                // is also removed. However, because first-discovery runs on the client
+                // and does not make use of "require" it cannot locate the copy of Infusion.
+                // The reinstallation of first-discovery puts back its copy of Infusion.
                 child_process.spawn("grunt", ["npm-install:first-discovery"], {stdio: "inherit"});
             }
         });
