299c69da6324f8f65ad773770d646ddd8f52e1de
Justin Obara <obarajustin@gmail.com>
2015-11-09 10:11:39 -0500

    FLOE-430: Connected to security server
    Needs a lot of refactoring and tests. This is just the first end-to-end working implementation.

--- src/js/preferencesRouter.js
+++ src/js/preferencesRouter.js
@@ -14,9 +14,28 @@ var fluid = require("infusion");
 var gpii = fluid.registerNamespace("gpii");
 
 require("gpii-express");
+require("./dataSource.js");
 
 fluid.defaults("gpii.firstDiscovery.server.preferences.handler", {
     gradeNames: ["gpii.express.handler"],
+    components: {
+        accessTokenDataSource: {
+            type: "gpii.firstDiscovery.server.dataSource.security",
+            options: {
+                url: "http://10.0.2.2:8081/access_token"
+            }
+        },
+        preferencesDataSource: {
+            type: "kettle.dataSource.URL",
+            options: {
+                writable: true,
+                url: "http://10.0.2.2:8081/add-preferences?view=%view",
+                termMap: {
+                    view: "%view"
+                }
+            }
+        }
+    },
     invokers: {
         handleRequest: {
             funcName: "gpii.firstDiscovery.server.preferences.handler.handleRequest",
@@ -25,14 +44,59 @@ fluid.defaults("gpii.firstDiscovery.server.preferences.handler", {
     }
 });
 
+gpii.firstDiscovery.server.preferences.prefrencesWrapper = {
+    "contexts": {
+        "gpii-default": {
+            "name": "Default preferences",
+            "preferences": {}
+        }
+    }
+};
+
 //TODO: This is just a rough in for the actual handler. This should be updated
 // to contact the security layer to request creation of a gpii-token and saving
 // of the prefrences set.
 gpii.firstDiscovery.server.preferences.handler.handleRequest = function (that) {
     var view = that.request.query.view || "";
-    that.sendResponse(200, {token: "mock-gpii-token:" + view});
-    // in the case of an error, the appropriate error code should be sent with a
-    // response that contains {error: "some error message"}
+    var body = that.request.body || {};
+
+    var accessTokenModel = {
+        grant_type: "client_credentials",
+        scope: "add_preferences",
+        client_id: "client_first_discovery",
+        client_secret: "client_secret_firstDiscovery"
+    };
+
+    var accessTokenPromise = that.accessTokenDataSource.set(null, accessTokenModel, {
+        writeMethod: "POST"
+    });
+
+    accessTokenPromise.then(function (response) {
+        var access = JSON.parse(response);
+
+        var preferences = fluid.copy(gpii.firstDiscovery.server.preferences.prefrencesWrapper);
+        fluid.set(preferences, ["contexts", "gpii-default", "preferences"], body);
+
+        var preferencesPromise = that.preferencesDataSource.set({
+            view: view
+        }, preferences, {
+            writeMethod: "POST",
+            headers: {
+                Authorization: access.token_type + " " + access.access_token
+            }
+        });
+
+        preferencesPromise.then(function (response) {
+            that.sendResponse(200, response);
+        }, function (error) {
+            var errorCode = error.statusCode || 500;
+            that.sendResponse(errorCode, error);
+        });
+
+    }, function (error) {
+        var errorCode = error.statusCode || 500;
+        that.sendResponse(errorCode, error);
+    });
 };
 
 fluid.defaults("gpii.firstDiscovery.server.preferences.router", {
